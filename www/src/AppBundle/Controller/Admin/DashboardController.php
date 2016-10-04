0d7846fd8884317a50871539af2765cdd1433e84
Hugo Piso <hugopiso@yahoo.com>
2016-10-04 18:31:04 -0400

    add template front + admin

---
+++ www/src/AppBundle/Controller/Admin/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DashboardController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('AppBundle:Admin/Dashboard:index.html.twig');
+    }
+}
