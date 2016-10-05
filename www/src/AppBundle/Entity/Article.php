1311d9a9819336c369084df94d0196202fb9a953
Hugo Piso <hugopiso@yahoo.com>
2016-10-05 09:54:17 -0400

    add category

--- www/src/AppBundle/Entity/Article.php
+++ www/src/AppBundle/Entity/Article.php
@@ -49,6 +49,12 @@ class Article
      */
     private $enabled;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="Category")
+     * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
+     */
+    private $category;
+
     public function __construct()
     {
         $this->createdAt = new \DateTime();
@@ -159,5 +165,29 @@ class Article
     public function getEnabled()
     {
         return $this->enabled;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \AppBundle\Entity\Category $category
+     *
+     * @return Article
+     */
+    public function setCategory(\AppBundle\Entity\Category $category = null)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \AppBundle\Entity\Category
+     */
+    public function getCategory()
+    {
+        return $this->category;
     }
 }
